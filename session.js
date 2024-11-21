cdc4d5783b533c7bd3d2b73ba72a7207b1f7f918
AKNAWAB804 <aknawab804@gmail.com>
2024-11-21 09:54:44 +0500

    Update session.js

--- session.js
+++ session.js
@@ -21,5 +21,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "your session_id here",
+SESSION_ID: process.env.SESSION_ID || "ANJU-MD=HYF2QACA#0MrF9PkPTZ1lpocumvU9s0DXP6g_ZjXVVgBY7aqxrFk",
 };
